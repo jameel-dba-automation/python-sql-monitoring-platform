72c36984a72304cf251ed0c9644e88562ae42e53
Jameel Qureshi | SQL DBA Automation <qj88ahmed@gmail.com>
2026-05-20 11:35:20 +0530

    Update table_creation_scripts.sql

--- table_creation_scripts.sql
+++ table_creation_scripts.sql
@@ -21,3 +21,31 @@ CREATE TABLE dbo.FragmentationHistory
 
     page_count BIGINT
 );
+
+
+CREATE TABLE dbo.LongRunningQueriesHistory
+(
+    ID INT IDENTITY(1,1),
+
+    CaptureTime DATETIME DEFAULT GETDATE(),
+
+    InstanceName VARCHAR(200),
+
+    DatabaseName VARCHAR(200),
+
+    login_name VARCHAR(200),
+
+    host_name VARCHAR(200),
+
+    status VARCHAR(100),
+
+    command VARCHAR(200),
+
+    cpu_time INT,
+
+    ElapsedTimeSeconds INT,
+
+    logical_reads BIGINT,
+
+    QueryText NVARCHAR(MAX)
+);
